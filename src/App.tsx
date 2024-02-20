b52030d60267ac68b444ee9cecec5b511e80e4f1
Raghib Najmi <84832906+ragOP@users.noreply.github.com>
2024-02-21 01:03:34 +0530

    allc hanges

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         <Routes>
         
      
-          <Route path = "/spanmed1" element = {<Third_SP />} />
+        <Route index element={<Third_SP />} />
         
         
         </Routes>
